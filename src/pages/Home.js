28045bd8a283b9771ca932381537ee40a07b3cd6
thais <trevillon@jehann.fr>
2022-10-26 10:15:36 +0200

    :tada: useState

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,19 @@
 import {boardgames} from "../utils/Games";
 import Card from "../components/Card";
 import {FiltreBtn1, FiltreBtn2} from "../components/FiltreBtn";
+import {useState} from "react";
 
 const Home = () => {
-    const games = boardgames;
+    const [games, setGames] = useState(boardgames);
     const genres = ['strategy', 'card', 'calm', 'beautiful', 'fantasy', 'humor', 'cooperative'];
 
     const filter = function (filtre) {
         console.log(filtre);
         // TODO : terminer le filtre
+    }
+
+    const purge = () => {
+        setGames([]);
     }
 
 
@@ -29,7 +34,7 @@ const Home = () => {
         <main id='Home'>
         <h1>Accueil</h1>
 
-            <section>
+            <section className=" centered">
                 <h2>Mes Jeux</h2>
 
                 {/* Version 1 : props nommée */}
@@ -40,19 +45,26 @@ const Home = () => {
                 {/* Version 2 : props children */}
                 <div className="flex" id="divId">
                     {genres.map(g => <FiltreBtn2 key={g}>{g}</FiltreBtn2>)}
+                    <button className='danger' onClick={purge}>Purger</button>
                 </div>
 
-                <div className='grid centered'>
-                    {games.map(game => <Card game={game} key={game.id}/>)}
-                    {/*
+                {games.length ?
+                    (
+                        <div className='grid'>
+                            {games.map(game => <Card game={game} key={game.id}/>)}
+                        </div>
+                    ) :
+                    <p>Aucun jeu à afficher</p>
+                }
+            </section>
+
+
+            {/*
                     Cette syntaxe :
                           <Card2 game={g}/>
                     Est (quasi) la version React de :
                         {Card2(g)}
                     */}
-
-                </div>
-            </section>
         </main>
     );
 }
